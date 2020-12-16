80125ae05a1e31eb1b08ee2a88b383b8bf6d0a29
antoine2116 <51358897+antoine2116@users.noreply.github.com>
2020-12-16 11:49:02 +0100

    Ajoutez des fichiers projet.

---
+++ Facturation_V2/Shared/IBusinessData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facturation_V2.Shared
+{
+    public interface IBusinessData
+    {
+        List<Facture> lesFactures { get; }
+    }
+}
